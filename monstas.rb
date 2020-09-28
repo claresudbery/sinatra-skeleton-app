677f5c1a4442036403491fcb8fb9e5e09eb01f29
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 21:02:16 +0100

    Use cookie to store session info

--- monstas.rb
+++ monstas.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "erb"
 
+enable :sessions
+
 # This page created as part of this tutorial: http://webapps-for-beginners.rubymonstas.org/sinatra/params.html
 
 # To spin up this Sinatra server just run this in browser: ruby monstas.rb
@@ -17,6 +19,7 @@ def read_names
 end
 
 get "/monstas" do
+    @message = session[:message]
     @name = params["name"]
     @names = read_names
     erb :monstas
@@ -25,5 +28,6 @@ end
 post "/monstas" do
     @name = params["name"]
     store_name("names.txt", @name)
+    session[:message] = "Successfully stored the name #{@name}."
     redirect "/monstas?name=#{@name}"
 end
