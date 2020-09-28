fa457108601d43281f29e991db6fe7608e015ea2
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 23:44:33 +0100

    Use basic Rack::Test functionality

--- monstas.rb
+++ monstas.rb
@@ -1,71 +1,71 @@
-require "sinatra/base"
+require "sinatra"
 require "erb"
 
 # This page created as part of this tutorial: http://webapps-for-beginners.rubymonstas.org/sinatra/params.html
 
 # To spin up this Sinatra server just run this in browser: ruby monstas.rb
 
-class Application < Sinatra::Base
-    use Rack::MethodOverride
-    
-    enable :sessions
+enable :sessions
 
-    def store_name(filename, string)
-        File.open(filename, "a+") do |file|
-            file.puts(string)
-        end
+def store_name(filename, string)
+    File.open(filename, "a+") do |file|
+        file.puts(string)
+    end
+end
+
+def read_names
+    return [] unless File.exist?("names.txt")
+    File.read("names.txt").split("\n")
+end
+
+class NameValidator    
+    def initialize(name, names)
+    @name = name.to_s
+    @names = names
     end
 
-    def read_names
-        return [] unless File.exist?("names.txt")
-        File.read("names.txt").split("\n")
+    def valid?
+    validate
+    @message.nil?
     end
 
-    class NameValidator    
-        def initialize(name, names)
-        @name = name.to_s
-        @names = names
-        end
-    
-        def valid?
-        validate
-        @message.nil?
-        end
-    
-        def message
-        @message
-        end
-    
-        private
-    
-        def validate
-            if @name.empty?
-            @message = "You need to enter a name."
-            elsif @names.include?(@name)
-            @message = "#{@name} is already included in our list."
-            end
-        end
+    def message
+    @message
     end
 
-    get "/monstas" do
-        @message = session.delete(:message)
-        @name = params["name"]
-        @names = read_names
+    private
+
+    def validate
+        if @name.empty?
+        @message = "You need to enter a name."
+        elsif @names.include?(@name)
+        @message = "#{@name} is already included in our list."
+        end
+    end
+end
+
+get '/' do
+    "Hello World #{params[:name]}".strip
+end
+
+get "/monstas" do
+    @message = session.delete(:message)
+    @name = params["name"]
+    @names = read_names
+    erb :monstas
+end
+
+post "/monstas" do
+    @name = params["name"]
+    @names = read_names
+    validator = NameValidator.new(@name, @names)
+
+    if validator.valid?
+        store_name("names.txt", @name)
+        session[:message] = "Successfully stored the name #{@name}."
+        redirect "/monstas?name=#{@name}"
+    else
+        @message = validator.message
         erb :monstas
-    end
-
-    post "/monstas" do
-        @name = params["name"]
-        @names = read_names
-        validator = NameValidator.new(@name, @names)
-
-        if validator.valid?
-            store_name("names.txt", @name)
-            session[:message] = "Successfully stored the name #{@name}."
-            redirect "/monstas?name=#{@name}"
-        else
-            @message = validator.message
-            erb :monstas
-        end
     end
 end
--- spec/monstas_spec.rb
+++ spec/monstas_spec.rb
@@ -1,2 +1,31 @@
+ENV['APP_ENV'] = 'test'
+
 require "spec_helper"
 
+require_relative '../monstas'
+require 'rspec'
+require 'rack/test'
+
+RSpec.describe 'The HelloWorld App' do
+    include Rack::Test::Methods
+  
+    def app
+      Sinatra::Application
+    end
+  
+    it "says hello" do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('Hello World')
+    end
+  
+    it "gets basic monstas route successfully" do
+      get '/monstas'
+      expect(last_response).to be_ok
+    end
+  
+    it "shows a submit button" do
+      get '/monstas'
+      expect(last_response.body).to include('submit')
+    end
+end
