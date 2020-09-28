e4f95c22841c2c8ad9412fda38ca443a94a83e63
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 21:09:27 +0100

    Add input validation

--- monstas.rb
+++ monstas.rb
@@ -18,6 +18,32 @@ def read_names
     File.read("names.txt").split("\n")
 end
 
+class NameValidator    
+    def initialize(name, names)
+      @name = name.to_s
+      @names = names
+    end
+  
+    def valid?
+      validate
+      @message.nil?
+    end
+  
+    def message
+      @message
+    end
+  
+    private
+  
+    def validate
+        if @name.empty?
+          @message = "You need to enter a name."
+        elsif @names.include?(@name)
+          @message = "#{@name} is already included in our list."
+        end
+    end
+end
+
 get "/monstas" do
     @message = session.delete(:message)
     @name = params["name"]
@@ -27,7 +53,15 @@ end
 
 post "/monstas" do
     @name = params["name"]
-    store_name("names.txt", @name)
-    session[:message] = "Successfully stored the name #{@name}."
-    redirect "/monstas?name=#{@name}"
+    @names = read_names
+    validator = NameValidator.new(@name, @names)
+
+    if validator.valid?
+        store_name("names.txt", @name)
+        session[:message] = "Successfully stored the name #{@name}."
+        redirect "/monstas?name=#{@name}"
+    else
+        @message = validator.message
+        erb :monstas
+    end
 end
