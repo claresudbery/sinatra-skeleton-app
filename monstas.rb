9721897110262d0b90d3d47946d8285e1a873b96
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 23:33:33 +0100

    Attempt to get app working with rackup

--- monstas.rb
+++ monstas.rb
@@ -1,67 +1,71 @@
-require "sinatra"
+require "sinatra/base"
 require "erb"
-
-enable :sessions
 
 # This page created as part of this tutorial: http://webapps-for-beginners.rubymonstas.org/sinatra/params.html
 
 # To spin up this Sinatra server just run this in browser: ruby monstas.rb
 
-def store_name(filename, string)
-    File.open(filename, "a+") do |file|
-        file.puts(string)
-    end
-end
+class Application < Sinatra::Base
+    use Rack::MethodOverride
+    
+    enable :sessions
 
-def read_names
-    return [] unless File.exist?("names.txt")
-    File.read("names.txt").split("\n")
-end
-
-class NameValidator    
-    def initialize(name, names)
-      @name = name.to_s
-      @names = names
-    end
-  
-    def valid?
-      validate
-      @message.nil?
-    end
-  
-    def message
-      @message
-    end
-  
-    private
-  
-    def validate
-        if @name.empty?
-          @message = "You need to enter a name."
-        elsif @names.include?(@name)
-          @message = "#{@name} is already included in our list."
+    def store_name(filename, string)
+        File.open(filename, "a+") do |file|
+            file.puts(string)
         end
     end
-end
 
-get "/monstas" do
-    @message = session.delete(:message)
-    @name = params["name"]
-    @names = read_names
-    erb :monstas
-end
+    def read_names
+        return [] unless File.exist?("names.txt")
+        File.read("names.txt").split("\n")
+    end
 
-post "/monstas" do
-    @name = params["name"]
-    @names = read_names
-    validator = NameValidator.new(@name, @names)
+    class NameValidator    
+        def initialize(name, names)
+        @name = name.to_s
+        @names = names
+        end
+    
+        def valid?
+        validate
+        @message.nil?
+        end
+    
+        def message
+        @message
+        end
+    
+        private
+    
+        def validate
+            if @name.empty?
+            @message = "You need to enter a name."
+            elsif @names.include?(@name)
+            @message = "#{@name} is already included in our list."
+            end
+        end
+    end
 
-    if validator.valid?
-        store_name("names.txt", @name)
-        session[:message] = "Successfully stored the name #{@name}."
-        redirect "/monstas?name=#{@name}"
-    else
-        @message = validator.message
+    get "/monstas" do
+        @message = session.delete(:message)
+        @name = params["name"]
+        @names = read_names
         erb :monstas
+    end
+
+    post "/monstas" do
+        @name = params["name"]
+        @names = read_names
+        validator = NameValidator.new(@name, @names)
+
+        if validator.valid?
+            store_name("names.txt", @name)
+            session[:message] = "Successfully stored the name #{@name}."
+            redirect "/monstas?name=#{@name}"
+        else
+            @message = validator.message
+            erb :monstas
+        end
     end
 end
