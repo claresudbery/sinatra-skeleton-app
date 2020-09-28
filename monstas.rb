f924e2ec68bf5858c824896c369dcf110b488e79
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 20:53:36 +0100

    Display list of names

--- monstas.rb
+++ monstas.rb
@@ -11,8 +11,19 @@ def store_name(filename, string)
     end
 end
 
+def read_names
+    return [] unless File.exist?("names.txt")
+    File.read("names.txt").split("\n")
+end
+
 get "/monstas" do
     @name = params["name"]
+    @names = read_names
     store_name("names.txt", @name)
     erb :monstas
 end
+
+# post "/monstas" do
+#     @name = params["name"]
+#     store_name("names.txt", @name)
+# end
